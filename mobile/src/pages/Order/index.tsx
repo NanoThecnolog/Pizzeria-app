eaea60360f88456e1866a609ed21d876cdbab561
Ericsson Gomes <contato@ericssongomes.com>
2024-05-08 14:12:06 -0300

    Feat Finishing mobile

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, TextInput, Modal } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, TextInput, Modal, FlatList } from "react-native";
 
 import { useRoute, RouteProp, useNavigation } from "@react-navigation/native";
 
@@ -7,6 +7,9 @@ import { Feather } from '@expo/vector-icons'
 
 import { api } from "../../services/api";
 import { ModalPicker } from "../../components/ModalPicker";
+import { ListItem } from "../../components/ListItems";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { StackPramsList } from "../../routes/app.routes";
 
 type RouteDetailParams = {
     Order: {
@@ -21,17 +24,33 @@ export type CategoryProps = {
     name: string;
 }
 
+type ProductProps = {
+    id: string;
+    name: string;
+}
+type ItemProps = {
+    id: string;
+    product_id: string;
+    name: string;
+    amount: string | number;
+}
+
 type OrderRouteProps = RouteProp<RouteDetailParams, 'Order'>;
 
 export default function Order() {
     const route = useRoute<OrderRouteProps>();
-    const navigation = useNavigation();
+    const navigation = useNavigation<NativeStackNavigationProp<StackPramsList>>();
 
     const [category, setCategory] = useState<CategoryProps[] | []>([]);
     const [categorySelected, setCategorySelected] = useState<CategoryProps>();
     const [modalVisible, setModalVisible] = useState(false)
 
-    const [amount, setAmount] = useState('1')
+    const [products, setProducts] = useState<ProductProps[] | []>([]);
+    const [productSelected, setProductSelected] = useState<ProductProps | undefined>()
+    const [modalProductVisible, setModalProductVisible] = useState(false);
+
+    const [amount, setAmount] = useState('1');
+    const [items, setItems] = useState<ItemProps[]>([])
 
     useEffect(() => {
         async function loadInfo() {
@@ -43,6 +62,20 @@ export default function Order() {
         loadInfo();
 
     }, [])
+
+    useEffect(() => {
+        async function loadProducts() {
+            const response = await api.get('/category/product', {
+                params: {
+                    category_id: categorySelected?.id
+                }
+            })
+            setProducts(response.data)
+            setProductSelected(response.data[0])
+        }
+        loadProducts();
+
+    }, [categorySelected])
 
 
     async function handleCloseOrder() {
@@ -64,6 +97,47 @@ export default function Order() {
     function handleChangeCategory(item: CategoryProps) {
         setCategorySelected(item);
     }
+    function handleChangeProduct(item: ProductProps) {
+        setProductSelected(item);
+    }
+    async function handleAdd() {
+        const response = await api.post('/order/add', {
+            order_id: route.params?.order_id,
+            product_id: productSelected?.id,
+            amount: Number(amount)
+        })
+        let data = {
+            id: response.data.id,
+            product_id: productSelected?.id as string,
+            name: productSelected?.name as string,
+            amount: amount
+        }
+
+        setItems(oldArray => [...oldArray, data])
+
+    }
+    async function handleDeleteItem(item_id: string) {
+        await api.delete('/order/remove', {
+            params: {
+                item_id: item_id
+            }
+        })
+
+        let removeItem = items.filter(item => {
+            return (item.id !== item_id)
+        })
+
+        setItems(removeItem);
+
+    }
+
+    function handleFinishOrder() {
+        navigation.navigate('FinishOrder', {
+            number: route.params?.number,
+            order_id: route.params?.order_id,
+
+        })
+    }
 
 
 
@@ -71,9 +145,11 @@ export default function Order() {
         <View style={styles.container}>
             <View style={styles.header}>
                 <Text style={styles.title}>Mesa {route.params.number}</Text>
-                <TouchableOpacity onPress={handleCloseOrder}>
-                    <Feather name="trash-2" size={28} color="#FF3f4b" />
-                </TouchableOpacity>
+                {items.length === 0 && (
+                    <TouchableOpacity onPress={handleCloseOrder}>
+                        <Feather name="trash-2" size={28} color="#FF3f4b" />
+                    </TouchableOpacity>
+                )}
 
             </View>
             {category.length !== 0 && (
@@ -84,9 +160,11 @@ export default function Order() {
                 </TouchableOpacity>
             )}
 
-            <TouchableOpacity style={styles.input}>
-                <Text style={{ color: '#fff' }}>pizza de calabreza</Text>
-            </TouchableOpacity>
+            {products.length !== 0 && (
+                <TouchableOpacity style={styles.input} onPress={() => setModalProductVisible(true)}>
+                    <Text style={{ color: '#fff' }}>{productSelected?.name}</Text>
+                </TouchableOpacity>
+            )}
 
             <View style={styles.qtdContainer}>
                 <Text style={styles.qtdText}>Quantidade</Text>
@@ -101,14 +179,22 @@ export default function Order() {
             </View>
 
             <View style={styles.actions}>
-                <TouchableOpacity style={styles.buttonAdd}>
+                <TouchableOpacity style={styles.buttonAdd} onPress={handleAdd}>
                     <Text style={styles.buttonText}>+</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity style={[styles.button, { opacity: items.length === 0 ? 0.5 : 1 }]} disabled={items.length === 0} onPress={handleFinishOrder}>
                     <Text style={styles.buttonText}>Avançar</Text>
 
                 </TouchableOpacity>
             </View>
+
+            <FlatList
+                showsVerticalScrollIndicator={false}
+                style={{ flex: 1, marginTop: 24 }}
+                data={items}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => <ListItem data={item} deleteItem={handleDeleteItem} />}
+            />
 
             <Modal transparent={true} visible={modalVisible} animationType="fade">
                 <ModalPicker
@@ -116,6 +202,14 @@ export default function Order() {
                     options={category}
                     selectedItem={handleChangeCategory}
                 />
+            </Modal>
+            <Modal transparent={true} visible={modalProductVisible} animationType="fade">
+                <ModalPicker
+                    handleCloseModal={() => setModalProductVisible(false)}
+                    options={products}
+                    selectedItem={handleChangeProduct}
+                />
+
             </Modal>
 
         </View>
--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -3,10 +3,15 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Dashboard from "../pages/Dashboard";
 import Order from "../pages/Order";
+import { FinishOrder } from "../pages/FinishOrder";
 
 export type StackPramsList = {
     Dashboard: undefined;
     Order: {
+        number: number | string;
+        order_id: string;
+    };
+    FinishOrder: {
         number: number | string;
         order_id: string;
     };
@@ -19,6 +24,7 @@ function AppRoutes() {
         <Stack.Navigator>
             <Stack.Screen name="Dashboard" component={Dashboard} options={{ headerShown: false }} />
             <Stack.Screen name="Order" component={Order} options={{ headerShown: false }} />
+            <Stack.Screen name="FinishOrder" component={FinishOrder} options={{ title: 'Finalizando', headerStyle: { backgroundColor: '#1d1d2e' }, headerTintColor: '#fff' }} />
         </Stack.Navigator>
 
 
